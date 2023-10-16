ca651e88abd90e3bbeb03a061ae4e490ccbc0786
Daniel1998-cenec <danielcesar1998@hotmail.com>
2023-10-16 19:32:54 +0200

    algo he hehco pero no esta terminado

--- apuntes.16.10/src/main/java/primera/actividad/Actividad5.java
+++ apuntes.16.10/src/main/java/primera/actividad/Actividad5.java
@@ -8,12 +8,36 @@ public class Actividad5 {
 		boolean juegoTerminado=false;
 		
 		do {
-			String nombre=JOptionPane.showInputDialog(null,"Dime como te llamas");
-			if(nombre==null) {
-				JOptionPane.showMessageDialog(null, "Le has dado al boton cancelar");
-				break;
-			}
-			String edad=JOptionPane.showInputDialog(null,"Dime cuantos años tienes");
-		}while(!juegoTerminado);
+			int opcion =JOptionPane.showConfirmDialog(null, "¿Te quieres registar?","Continuar", JOptionPane.YES_NO_OPTION);
+			if (opcion == JOptionPane.YES_OPTION) {
+				String nombre=JOptionPane.showInputDialog(null,"Dime como te llamas");
+				if(nombre!=null) {
+					JOptionPane.showMessageDialog(null, "Felicidades tu nombre es, "+nombre);
+				}else {
+					JOptionPane.showMessageDialog(null, "Le has dado al boton cancelar");
+					break;
+				}
+				
+				if(nombre==" ") {
+					JOptionPane.showMessageDialog(null, "Tu "+nombre+" no puede estar vacio");
+					juegoTerminado=false;
+				}else {
+					String edad=JOptionPane.showInputDialog(null,"Dime cuantos años tienes");
+					if(edad!=null) {
+						JOptionPane.showMessageDialog(null, "Felicidades tu edad es, "+edad);
+					}else {
+						JOptionPane.showMessageDialog(null, "Le has dado al boton cancelar");
+						break;
+					}
+					JOptionPane.showMessageDialog(null, "El nombre del usuario es, "+nombre+
+							" y su edad es, "+edad);
+				}	
+			} else if (opcion == JOptionPane.NO_OPTION) {
+				JOptionPane.showMessageDialog(null, "Has elegido No.");
+			}	
+			
+		}while(juegoTerminado);
 	}
 }
+
+//Sin terminar.
