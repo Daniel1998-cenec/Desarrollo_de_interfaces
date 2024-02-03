8e84bd9ffd802597e42f96a1ed7730451ea72d30
Daniel1998-cenec <danielcesar1998@hotmail.com>
2024-02-03 17:40:08 +0100

    Todas las actividades realizada.

--- SEGUNDOTRIMESTRE/Ejercicios2201/src/module-info.java
+++ SEGUNDOTRIMESTRE/Ejercicios2201/src/module-info.java
@@ -1,8 +1,12 @@
 module EjerciciosUnidad5Controles {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens ejercicio1 to javafx.graphics, javafx.fxml;
 	opens ejercicio2 to javafx.graphics, javafx.fxml;
+	opens ejercicio34y5 to javafx.graphics, javafx.fxml;
+	opens ejercicio6 to javafx.graphics, javafx.fxml;
 }
